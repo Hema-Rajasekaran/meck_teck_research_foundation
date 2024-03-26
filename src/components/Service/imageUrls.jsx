84411d963e3fa1b39669e3d4da8ab4c329e054c2
Hema-Rajasekran R <hema.rajasekaran03@outlook.com>
2024-03-26 02:50:55 -0700

    first commit

---
+++ src/components/Service/imageUrls.jsx
@@ -0,0 +1,16 @@
+import slide_image_6 from "../../images/i1.jpeg";
+import slide_image_7 from "../../images/i2.jpeg";
+import slide_image_8 from "../../images/i3.jpeg";
+import slide_image_9 from "../../images/i4.jpeg";
+
+
+const imageUrls = [
+  slide_image_7,
+  slide_image_8,
+  slide_image_9,
+  // slide_image_10,
+  // slide_image_5,
+  slide_image_6,
+];
+
+export default imageUrls;
